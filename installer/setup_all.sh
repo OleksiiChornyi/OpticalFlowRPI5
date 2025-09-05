fd4973ee10340ecf0e4fdbb12867bb33e3a0c8bd
OleksiiChornyi <black20011106alex@gmail.com>
2025-09-05 14:21:10 +0000

    Change paths

--- installer/setup_all.sh
+++ installer/setup_all.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 chmod +x ./OpticalFlowRPI5/installer/install_ros2_mavros.sh 
-./install_ros2_mavros.sh
+./OpticalFlowRPI5/installer/install_ros2_mavros.sh 
 
 chmod +x ./OpticalFlowRPI5/installer/install_camera_deps.sh 
-./install_camera_deps.sh
+./OpticalFlowRPI5/installer/install_camera_deps.sh 
 
 chmod +x ./OpticalFlowRPI5/installer/create_optical_flow.py.sh 
-./create_optical_flow.py.sh
+./OpticalFlowRPI5/installer/create_optical_flow.py.sh 
 
 chmod +x ./OpticalFlowRPI5/installer/create_run_scripts.sh
-./create_run_scripts.sh
+./OpticalFlowRPI5/installer/create_run_scripts.sh
 
 # Tu run tmux
 # ~/autorun/run_all_tmux.sh
